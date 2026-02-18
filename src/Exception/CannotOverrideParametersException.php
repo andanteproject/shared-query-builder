46469b4362bc7d8a91919df580e215441e668c50
Cristoforo Cervino <cristoforo.cervino@me.com>
2026-02-18 22:49:09 +0100

    update cs-fixer

--- src/Exception/CannotOverrideParametersException.php
+++ src/Exception/CannotOverrideParametersException.php
@@ -6,7 +6,7 @@ namespace Andante\Doctrine\ORM\Exception;
 
 class CannotOverrideParametersException extends ImmutableParameterException
 {
-    public function __construct(int $code = 0, \Throwable $previous = null)
+    public function __construct(int $code = 0, ?\Throwable $previous = null)
     {
         parent::__construct(
             'Cannot override query parameters because some defined parameters are immutable',
